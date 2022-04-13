920da38a450e7e88aeb71a5597a94bf826734495
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-04-13 09:59:50 +0900

    add search button onclick

--- app/src/main/java/com/mobileteam/laundry/MainActivity.java
+++ app/src/main/java/com/mobileteam/laundry/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // haha
+
+        // 검색 버튼 onClick
+        ImageButton searchButton = (ImageButton) findViewById(R.id.search_button);
+        searchButton.setOnClickListener(v -> {
+            onSearchClicked();
+        });
     }
 
-    public void onSearchClicked(View v) {
+    public void onSearchClicked() {
         Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
     }
