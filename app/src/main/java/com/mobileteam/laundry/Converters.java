9bcf9d92b557baf847657b0f7482940f70a0f03b
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-05-02 11:55:52 +0900

    add type converters

--- app/src/main/java/com/mobileteam/laundry/Converters.java
+++ app/src/main/java/com/mobileteam/laundry/Converters.java
@@ -5,6 +5,11 @@ import android.graphics.BitmapFactory;
 
 import androidx.room.TypeConverter;
 
+import com.mobileteam.laundry.enums.Detergent;
+import com.mobileteam.laundry.enums.Temperature;
+import com.mobileteam.laundry.enums.WashingPower;
+import com.mobileteam.laundry.enums.WashingType;
+
 import java.io.ByteArrayOutputStream;
 
 public class Converters {
@@ -23,5 +28,53 @@ public class Converters {
     public Bitmap toBitmap(byte[] byteArray) {
         if(byteArray == null) return null;
         return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+    }
+
+    // WashingType -> String
+    @TypeConverter
+    public String fromWashingType(WashingType washingType) {
+        return washingType.name();
+    }
+
+    // String -> WashingType
+    @TypeConverter
+    public WashingType toWashingType(String str) {
+        return WashingType.valueOf(str);
+    }
+
+    // WashingPower -> String
+    @TypeConverter
+    public String fromWashingPower(WashingPower washingPower) {
+        return washingPower.name();
+    }
+
+    // String -> WashingPower
+    @TypeConverter
+    public WashingPower toWashingPower(String str) {
+        return WashingPower.valueOf(str);
+    }
+
+    // Temperature -> String
+    @TypeConverter
+    public String fromTemperature(Temperature temperature) {
+        return temperature.name();
+    }
+
+    // String -> Temperature
+    @TypeConverter
+    public Temperature toTemperature(String str) {
+        return Temperature.valueOf(str);
+    }
+
+    // Detergent -> String
+    @TypeConverter
+    public String fromDetergent(Detergent detergent) {
+        return detergent.name();
+    }
+
+    // String -> Detergent
+    @TypeConverter
+    public Detergent toDetergent(String str) {
+        return Detergent.valueOf(str);
     }
 }
