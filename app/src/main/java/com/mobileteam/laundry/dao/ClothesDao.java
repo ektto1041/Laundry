f02d5f4adc1277edf4cdfae8d213c4e12085b7e6
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-05-07 21:24:37 +0900

    color update

--- app/src/main/java/com/mobileteam/laundry/dao/ClothesDao.java
+++ app/src/main/java/com/mobileteam/laundry/dao/ClothesDao.java
@@ -5,6 +5,7 @@ import androidx.room.Insert;
 import androidx.room.Query;
 
 import com.mobileteam.laundry.domain.Clothes;
+import com.mobileteam.laundry.enums.ClothesColor;
 import com.mobileteam.laundry.enums.Detergent;
 import com.mobileteam.laundry.enums.Temperature;
 import com.mobileteam.laundry.enums.WashingPower;
@@ -12,7 +13,6 @@ import com.mobileteam.laundry.enums.WashingType;
 
 import java.util.List;
 
-import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Single;
 
 @Dao
@@ -24,7 +24,17 @@ public interface ClothesDao {
     public Single<List<Clothes>> getAll();
 
     @Query("SELECT * FROM CLOTHES " +
+            "INNER JOIN TEXTURES ON CLOTHES.id = TEXTURES.clothes_id " +
             "WHERE washing_type = :washingType AND washing_power = :washingPower" +
-            " AND detergent = :detergent AND temperature = :temperature")
-    public Single<List<Clothes>> findAll(WashingType washingType, WashingPower washingPower, Detergent detergent, Temperature temperature);
+            " AND detergent = :detergent AND temperature = :temperature" +
+            " AND colors IN (:clothesColorList)" +
+            " AND TEXTURES.name IN (:textureList)")
+    public Single<List<Clothes>> findAll(
+            WashingType washingType,
+            WashingPower washingPower,
+            Detergent detergent,
+            Temperature temperature,
+            List<ClothesColor> clothesColorList,
+            List<String> textureList
+    );
 }
---
+++ app/src/main/java/com/mobileteam/laundry/enums/ClothesColor.java
@@ -0,0 +1,13 @@
+package com.mobileteam.laundry.enums;
+
+public enum ClothesColor {
+    WHITE,
+    GRAY,
+    BLACK,
+    RED,
+    GREEN,
+    BLUE,
+    CYAN,
+    YELLOW,
+    MAGENTA
+}
